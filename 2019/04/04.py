f6c8dceabd87d64be7c74f445fa7b19df7f54090
Magnus Heino <magnus.heino@lm.se>
2019-12-04 09:57:29 +0100

    opt

--- 2019/04/04.py
+++ 2019/04/04.py
@@ -1,15 +1,20 @@
 import re
 
 pattern = re.compile(r"(1{2,}|2{2,}|3{2,}|4{2,}|5{2,}|6{2,}|7{2,}|8{2,}|9{2,}|0{2,})")
-count = 0
-for password in range(353096, 843212):
-    if any(len(match) == 2 for match in pattern.findall(str(password))):
-        increasing = True
-        current = 0
-        for digit in map(int, list(str(password))):
-            if digit >= current:
-                current = digit
-            else:
-                increasing = False
-        count += 1 if increasing else 0
-print(count)
+increasing = re.compile(r"^\d*(?=\d{6}(\d*)$)0*1*2*3*4*5*6*7*8*9*\1$")
+
+
+def count(min_group):
+    count = 0
+    for password in map(str, range(353096, 843212)):
+        if any(min_group(match) for match in pattern.findall(password)):
+            count += 1 if increasing.findall(password) else 0
+    return count
+
+
+print(
+    "Part 1:",
+    count(lambda match: match),
+    "Part 2:",
+    count(lambda match: len(match) == 2),
+)
