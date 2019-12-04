09c6dc18ab7b673e56470cc460e65887981c3e3a
Magnus Heino <magnus.heino@lm.se>
2019-12-04 09:59:45 +0100

    opt

--- 2019/04/04.py
+++ 2019/04/04.py
@@ -4,10 +4,10 @@ pattern = re.compile(r"(1{2,}|2{2,}|3{2,}|4{2,}|5{2,}|6{2,}|7{2,}|8{2,}|9{2,}|0{
 increasing = re.compile(r"^\d*(?=\d{6}(\d*)$)0*1*2*3*4*5*6*7*8*9*\1$")
 
 
-def count(min_group):
+def count(match_condition):
     count = 0
     for password in map(str, range(353096, 843212)):
-        if any(min_group(match) for match in pattern.findall(password)):
+        if any(match_condition(match) for match in pattern.findall(password)):
             count += 1 if increasing.findall(password) else 0
     return count
 
